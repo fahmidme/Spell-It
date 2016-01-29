da90f47952a1b32c29bbb3de68da8c76953c4395
fahmidme <F@uws150598usr.uws.ualberta.ca>
2016-01-28 23:33:21 -0700

    First
    Starting

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Fade : MonoBehaviour {
+
+	public float fadeSpeed = 1.5f;
+
+	private bool sceneStarting = true;
+
+	void Awake () {
+		// Set the texture so that it is the the size of the screen and covers it.
+		GetComponent<GUITexture>().pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		// If the scene is starting...
+		if(sceneStarting)
+			// ... call the StartScene function.
+			StartScene();
+	}
+
+
+	void FadeToClear ()
+	{
+		// Lerp the colour of the texture between itself and transparent.
+		GetComponent<GUITexture>().color = Color.Lerp(GetComponent<GUITexture>().color, Color.clear, fadeSpeed * Time.deltaTime);
+	}
+
+
+	void FadeToBlack ()
+	{
+		// Lerp the colour of the texture between itself and black.
+		GetComponent<GUITexture>().color = Color.Lerp(GetComponent<GUITexture>().color, Color.black, fadeSpeed * Time.deltaTime);
+	}
+
+
+	void StartScene ()
+	{
+		// Fade the texture to clear.
+		FadeToClear();
+
+		// If the texture is almost clear...
+		if(GetComponent<GUITexture>().color.a <= 0.05f)
+		{
+			// ... set the colour to clear and disable the GUITexture.
+			GetComponent<GUITexture>().color = Color.clear;
+			GetComponent<GUITexture>().enabled = false;
+
+			// The scene is no longer starting.
+			sceneStarting = false;
+		}
+	}
+
+
+	public void EndScene (int level)
+	{
+		// Make sure the texture is enabled.
+		GetComponent<GUITexture>().enabled = true;
+
+		// Start fading towards black.
+		FadeToBlack();
+
+		// If the screen is almost black...
+		if(GetComponent<GUITexture>().color.a >= 0.95f)
+			// ... reload the level.
+			Application.LoadLevel(level);
+	}
+}
+
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public Button playBtn;
+	public AudioSource buttonIn;
+	public AudioSource buttonOut;
+
+	private Vector2 playBtnInitDimen;
+
+	// Use this for initialization
+	void Start () {
+		playBtnInitDimen = playBtn.image.rectTransform.sizeDelta;
+	}
+
+	void Update () {
+
+	}
+
+	public void touchIn() {
+		playBtn.image.rectTransform.sizeDelta = new Vector2 (playBtnInitDimen.x + 20, playBtnInitDimen.y + 20);
+		buttonOut.Stop ();
+		buttonIn.Play ();
+	}
+
+	public void touchOut() {
+		playBtn.image.rectTransform.sizeDelta = new Vector2 (playBtnInitDimen.x - 20, playBtnInitDimen.y - 20);
+		buttonIn.Stop ();
+		buttonOut.Play ();
+	}
+
+	void fadeIn() {
+
+	}
+}
